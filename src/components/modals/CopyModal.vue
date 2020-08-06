9d35daf33013c708db768f9b2d9d6273e986e347
Ilham <SergeyGuliy>
2020-08-06 06:08:57 +0300

    develop create lesson logic

---
+++ src/components/modals/CopyModal.vue
@@ -0,0 +1,129 @@
+<template>
+  <v-modal-window>
+    <div class="create-exit-box">
+      <div class="create-exit-box__view">
+        <img src="../../assets/exit-modal.svg" alt="" />
+      </div>
+      <div class="create-exit-box__title">Ссылка скопирована в буфер обмена</div>
+      <a class="create-exit-box__caption link" :href="$store.state.modal.data" target="_blank">
+        {{ $store.state.modal.data }}
+      </a>
+      <div class="create-exit-box__actions">
+        <div class="create-exit-box__action">
+          <v-btn @click="close()" type="button">ОК</v-btn>
+        </div>
+      </div>
+    </div>
+  </v-modal-window>
+</template>
+
+<script>
+import modal from "../../mixins/modal";
+
+export default {
+  name: "LandingCloseModal",
+  mixins: [modal]
+};
+</script>
+
+<style lang="scss">
+@media (max-width: 834px) {
+  .modal-window.open {
+    background-color: #595d61;
+  }
+  .modal-window__exit {
+    display: none;
+  }
+  .modal-window__wrapper {
+    padding: 0;
+    background-color: #e1e2e2;
+    overflow: hidden;
+    padding-top: 20px;
+  }
+  .create-exit-box__action.close button {
+    color: #58606e !important;
+    &:hover {
+      border: 1px solid #8e99ab;
+    }
+  }
+  .create-exit-box__action {
+    flex: 1 1 100%;
+    width: 100%;
+    padding: 0;
+    button {
+      width: 100%;
+      border-radius: 0;
+      border: 1px solid #8e99ab;
+      background-color: #e1e2e2 !important;
+      color: #faac15;
+      font-weight: 500;
+      padding: 0;
+    }
+  }
+  .create-exit-box__title {
+    font-weight: bold;
+    font-size: 16px;
+    line-height: 20px;
+    color: #1d2228;
+  }
+
+  .create-exit-box__view {
+    display: none;
+  }
+  .create-exit-box__caption {
+    margin-top: 16px;
+    display: block;
+    text-decoration: underline;
+    cursor: pointer;
+    &:hover {
+      color: #1d2228 !important;
+      text-decoration: underline;
+    }
+  }
+}
+
+@media (max-width: 540px) {
+  .modal-window__wrapper {
+    margin: 0 52px;
+  }
+  .create-exit-box__title,
+  .create-exit-box__caption {
+    margin: 0 5px;
+  }
+}
+@media (max-width: 450px) {
+  .modal-window__wrapper {
+    margin: 0 30px;
+  }
+}
+@media (max-width: 375px) {
+  .modal-window__wrapper {
+    margin: 0 20px;
+  }
+}
+.create-exit-box__caption.link {
+  margin-top: 16px;
+  display: block;
+  text-decoration: underline;
+  cursor: pointer;
+  &:hover {
+    color: #ffc107;
+    text-decoration: underline;
+  }
+}
+.create-exit-box__actions {
+  margin-top: 26px;
+}
+
+.create-exit-box__title {
+  padding: 0 85px;
+  font-family: "Roboto", sans-serif;
+  font-weight: 500;
+  font-size: 20px;
+  line-height: 28px;
+  color: #1d2228;
+  @media (max-width: 834px) {
+    padding: 0;
+  }
+}
+</style>
